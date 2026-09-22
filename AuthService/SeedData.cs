da1331b596c6a19ef6055aca659090ac1e8eaaa9
MedKhalil95 <jguirimmohamedkhalil95@gmail.com>
2026-09-22 06:55:22 +0100

    Adding new employees
    Signed-off-by: MedKhalil95 <jguirimmohamedkhalil95@gmail.com>

--- AuthService/SeedData.cs
+++ AuthService/SeedData.cs
@@ -14,7 +14,7 @@ public static class SeedData
     {
         new UserAccount { Username = "manager", Password = "Manager@2026", Role = "manager", DisplayName = "المدير", Phone = "" },
         new UserAccount { Username = "hossam", Password = "Hossam@123", Role = "employee", DisplayName = "حسام", Phone = "+216 20 000 001" },
-        new UserAccount { Username = "issam", Password = "issaù@123", Role = "employee", DisplayName = "عصام", Phone = "+216 20 000 002" },
+        new UserAccount { Username = "issam", Password = "issam@123", Role = "employee", DisplayName = "عصام", Phone = "+216 20 000 002" },
         new UserAccount { Username = "wajdi", Password = "Wajdi@123", Role = "employee", DisplayName = "وجدي", Phone = "+216 20 000 004" },
         new UserAccount { Username = "sadok", Password = "Sadok@123", Role = "employee", DisplayName = "الصادق", Phone = "+216 20 000 005" },
         new UserAccount { Username = "belgacem", Password = "Belgacem@123", Role = "employee", DisplayName = "بلڤاسم", Phone = "+216 20 000 006" },
@@ -22,7 +22,7 @@ public static class SeedData
         new UserAccount { Username = "moez", Password = "Moez@123", Role = "employee", DisplayName = "معز", Phone = "+216 20 000 008" },
         new UserAccount { Username = "nizar", Password = "Nizar@123", Role = "employee", DisplayName = "نزار", Phone = "+216 20 000 009" },
         new UserAccount { Username = "delegation", Password = "Delegation@2026", Role = "delegation", DisplayName = "الوفد الدبلوماسي", Phone = "" },
-        new UserAccount { Username= "Aazzeddine", Password= "Aazzeddine@123", Role= "employee", DisplayName= "عز الدين", Phone= "+216 20 000 010" },
+        new UserAccount { Username= "azzeddine", Password= "3ezzeddine@123", Role= "employee", DisplayName= "عز الدين", Phone= "+216 20 000 010" },
         };
 
     public const string AgencyLocationName = "CMAI Tunisia (Agency)";
